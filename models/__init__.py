b71ca5d21bb78d7844f9aecea407276987c4ef6d
Mutian Li <57753175+leinace1001@users.noreply.github.com>
2023-10-27 13:27:28 -0400

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from .transformer import MotionTransformer, InteractionMotionTransformer
+from .gaussian_diffusion import GaussianDiffusion
+
+__all__ = ['MotionTransformer', 'GaussianDiffusion', 'InteractionMotionTransformer']
